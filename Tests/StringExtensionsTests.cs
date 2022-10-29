87fb690083360d6161225cf00960bdc34248a1ab
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-10-29 18:09:22 -0300

    Criação de testes de unidade.

---
+++ Tests/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using StringExtensionPack;
+
+namespace Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("entity_framework_core")]
+        public void Text_WithUnderline_ReturnCamelCase(string text)
+        {
+            var result = text.ToCamelCase();
+            Assert.Equal("entityFrameworkCore", result);
+        }
+
+        [Theory]
+        [InlineData("entityFrameworkcore")]
+        [InlineData("dotnet-framework")]
+        [InlineData("oláMundo")]
+        public void Text_WithoutUnderline_ReturnLowered(string text)
+        {
+            var result = text.ToCamelCase();
+            Assert.Equal(text.ToLower(), result);
+        }
+    }
+}
